a7608bbbad3ed79184c7d5420cc00353c5dac81f
Stabile Frisur <26568863+stabilefrisur@users.noreply.github.com>
2025-03-08 15:16:42 +0000

    feat: enhance aggregate_fields_by_label function with preliminary results option and improved assertions

--- tests/test_aggregation.py
+++ tests/test_aggregation.py
@@ -17,17 +17,22 @@ def test_aggregate_fields_by_label_with_regex() -> None:
         "C_group": {"long": ("C.*", 1), "short": ("C1", -1)},
     }
     result = aggregate_fields_by_label(
-        df, "id", "weight", ["value1", "value2"], label_regex=regex_dict, method="wavg"
+        df,
+        id_column="id",
+        weight_column="weight",
+        field_columns=["value1", "value2"],
+        label_regex=regex_dict,
+        method="wavg",
     )
     print(result)
 
     test_result = pd.DataFrame(
         {
-            "group": ["A_group", "B_group", "C_group"],
-            "aggregated_value1": [55.0, 12.0, 10.0],
-            "aggregated_value2": [60.0, 12.0, 10.0],
+            "Group": ["A_group", "B_group", "C_group"],
+            "value1": [55.0, 12.0, 10.0],
+            "value2": [60.0, 12.0, 10.0],
         }
-    )
+    ).set_index("Group")
     assert result.equals(test_result), f"Expected {test_result}, but got {result}"
 
 
@@ -41,17 +46,22 @@ def test_aggregate_fields_by_label_with_label() -> None:
     }
     df = pd.DataFrame(data)
     result = aggregate_fields_by_label(
-        df, "id", "weight", ["value1", "value2"], label_column="label", method="wavg"
+        df,
+        id_column="id",
+        weight_column="weight",
+        field_columns=["value1", "value2"],
+        label_column="label",
+        method="wavg",
     )
     print(result)
 
     test_result = pd.DataFrame(
         {
-            "group": ["bar", "baz", "foo"],
-            "aggregated_value1": [890 / 15, 71.0, 54.0],
-            "aggregated_value2": [965 / 15, 76.0, 59.0],
+            "Group": ["foo", "bar", "baz"],
+            "value1": [54.0, 890 / 15, 71.0],
+            "value2": [59.0, 965 / 15, 76.0],
         }
-    )
+    ).set_index("Group")
     assert result.equals(test_result), f"Expected {test_result}, but got {result}"
 
 
@@ -63,16 +73,27 @@ def test_aggregate_fields_by_label_no_label_no_regex() -> None:
         "weight": [1, 2, 3, 4, 5, 6, 7, 8, 9],
     }
     df = pd.DataFrame(data)
-    result = aggregate_fields_by_label(df, "id", "weight", ["value1", "value2"], method="wavg")
+    df["id"] = pd.Categorical(
+        data["id"],
+        categories=["A0", "A2", "A1", "B0", "B1", "B2", "C0", "C1", "C2"],
+        ordered=True,
+    )
+    result = aggregate_fields_by_label(
+        df,
+        id_column="id",
+        weight_column="weight",
+        field_columns=["value1", "value2"],
+        method="wavg",
+    )
     print(result)
 
     test_result = pd.DataFrame(
         {
-            "group": ["A0", "A1", "A2", "B0", "B1", "B2", "C0", "C1", "C2"],
-            "aggregated_value1": [10.0, 40.0, 70.0, 20.0, 50.0, 80.0, 30.0, 60.0, 90.0],
-            "aggregated_value2": [15.0, 45.0, 75.0, 25.0, 55.0, 85.0, 35.0, 65.0, 95.0],
+            "Group": ["A0", "A2", "A1", "B0", "B1", "B2", "C0", "C1", "C2"],
+            "value1": [10.0, 70.0, 40.0, 20.0, 50.0, 80.0, 30.0, 60.0, 90.0],
+            "value2": [15.0, 75.0, 45.0, 25.0, 55.0, 85.0, 35.0, 65.0, 95.0],
         }
-    )
+    ).set_index("Group")
     assert result.equals(test_result), f"Expected {test_result}, but got {result}"
 
 
