a4c026c575c3fb3089ff9ec8ffcd39227f2fc599
Stabile Frisur <26568863+stabilefrisur@users.noreply.github.com>
2025-04-19 22:44:13 +0100

    fix: add logger functions to module exports

--- src/xsam/__init__.py
+++ src/xsam/__init__.py
@@ -1,5 +1,6 @@
 from .aggregation import aggregate_fields_by_label
 from .input import load
 from .output import save
+from .logger import get_file_logger, set_log_path
 
-__all__ = ["aggregate_fields_by_label", "save", "load"]
+__all__ = ["aggregate_fields_by_label", "save", "load", "get_file_logger", "set_log_path"]
